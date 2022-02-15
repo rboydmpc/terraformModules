5c50591dba4cf0d5b04d403248dd403aad814066
rboydmpc <73500396+rboydmpc@users.noreply.github.com>
2022-02-15 09:42:45 -0500

    pub

---
+++ AWS/simpleS3/main.tf
@@ -0,0 +1,13 @@
+resource "aws_s3_bucket" "s3" {
+    bucket = var.bucket_name
+
+    tags = {
+    Name = var.bucket_name
+    Date = timestamp()
+    }
+}
+
+resource "aws_s3_bucket_acl" "acl" {
+  bucket = aws_s3_bucket.s3.id
+  acl    = var.acl_value
+}
