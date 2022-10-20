41e9dacc3ad20f3369caff74d700a28c01128ca1
rboydmpc <73500396+rboydmpc@users.noreply.github.com>
2022-10-20 10:05:45 -0400

    Update main.tf

--- AWS/simpleS3/main.tf
+++ AWS/simpleS3/main.tf
@@ -7,7 +7,7 @@ resource "aws_s3_bucket" "s3" {
     }
 }
 
-resource "aws_s3_bucket_acl" "acl" {
-  bucket = aws_s3_bucket.s3.id
-  acl    = var.acl_value
-}
+#resource "aws_s3_bucket_acl" "acl" {
+#  bucket = aws_s3_bucket.s3.id
+#  acl    = var.acl_value
+#}
